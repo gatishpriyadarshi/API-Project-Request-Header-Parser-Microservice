9d789b1d7137dfd3472bb435b401748e7f7bc6f3
Gatish Priyadarshi <gatishpriyadarshi777@gmail.com>
2023-02-15 09:39:41 +0530

    changes

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', (req, res)=>{
+  const ipAddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ipAddress, language, software});
+})
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
